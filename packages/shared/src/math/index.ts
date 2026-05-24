165e362f3b17661a8fad6129147199a54f27b542
surumeneco <128275841+surumeneco@users.noreply.github.com>
2026-05-25 00:10:03 +0900

    feat: add math module index for exports

---
+++ packages/shared/src/math/index.ts
@@ -0,0 +1,10 @@
+// Math utilities - re-export all modules
+export * as matrix from './matrix'
+export * as vector from './vector'
+export * as stats from './stats'
+export * as array from './array'
+
+// Also export types
+export type { Matrix } from './matrix'
+export type { Vec } from './vector'
+export type { NDArray } from './array'
