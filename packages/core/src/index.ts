c9dda056585efcb9cfd365152c01ed4a72cafa89
surumeneco <128275841+surumeneco@users.noreply.github.com>
2026-05-24 23:54:07 +0900

    🧠 Add core entry point

---
+++ packages/core/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @tail-fairy/core
+ * Spiking Neural Network Core Engine with WebGPU acceleration
+ */
+
+export * from './neuron'
+export * from './synapse'
+export * from './learning'
+export * from './homeostasis'
+export * from './plasticity'
+export * from './models'
+export * from './gpu'
